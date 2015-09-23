94da98c13e223bd59f0237a13261be2b00c30e5b
pdillon89 <pauldillon@rcsi.ie>
2015-09-23 19:21:40 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+##download zip file##
+fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, destfile = "Dataset.zip", method = "curl")
+unzip("Dataset.zip")
+
+###read tables into memory###
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
+subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+y_train<-read.table("UCI HAR Dataset/train/Y_train.txt")
+activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
+features<-read.table("UCI HAR Dataset/features.txt")
+
+##label the variables##
+headers<-features[,2]
+names(x_test)<-headers
+names(x_train)<-headers
+
+##merge datatables
+x_merge<-rbind(x_train, x_test)
+y_merge<-rbind(y_train, y_test)
+subject_merge<-rbind(subject_train, subject_test)
+
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+x_merge<-x_merge[, mean_and_std_features]
+names(x_merge)<-mean_and_std_features[,2]
+
+merged_data<-cbind(subject_merge, y_merge, x_merge)
+names(merged_data)[1]<-"SubjectId"
+names(merged_data)[2]<-"Activity"
+
+##label activities values##
+merged_data$Activity<-factor(merged_data$Activity, labels=activity_labels[,2])
+
+##tidy dataset
+library(dplyr)
+tidy_data<-
+       group_by(merged_data, Activity, SubjectId) %>%
+       summarise(mean(3:66)) %>%
+       write.table("tidy_data.txt", row.name=FALSE)
+
+
+
